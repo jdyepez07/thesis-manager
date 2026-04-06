418e74efd02aa1a008e560a7461a02ce10cd1777
Juan David Yepez V. <yepesvelezjuandavid@gmail.com>
2026-04-05 22:54:01 -0500

    CORS bug fixed

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ import yaml
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
 
 @app.after_request
 def add_cors_headers(response):
